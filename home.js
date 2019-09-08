b5ce2e1ce95d0be41c07e32581e4df0e363869ab
Amit H Bhagat <amitbhagat.h@gmail.com>
2019-09-08 17:14:42 +0530

    Email id
    Passed in an email id to the function.

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
-(function(){
+(function(email){
   const fname = 'Amit';
   const lname = 'Bhagat';
   const fullName = () => fname + lname;
   console.log(`My name is ${fullName()}`);
-})();
+})('amitbhagat@gmail.com');
